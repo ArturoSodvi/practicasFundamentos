887a98ce9508aa1fe15f696f713eb742ae1f2dcb
ArturoSodvi <47364847+ArturoSodvi@users.noreply.github.com>
2020-11-17 12:35:07 -0600

    Practica7_ArturoPerez

---
+++ Practica7_ArturoPerez.c
@@ -0,0 +1,28 @@
+/*Práctica7                     Pérez De La Cruz Arturo
+Fecha de enrtrega: 17 de noviembre de 2020
+*/
+
+//Sección de librerías que se usarán en el código
+#include<stdio.h>
+
+//declaración de variables globales
+int edadDeAlumnos; 
+
+//declaración de funciones 
+int main(){
+	//declaración de variables locales
+	//uso de variables control del juego 
+	int enegiaPersonaje=10;
+	float puntaje=0;
+	char nivel='A';	
+	//Bloque de Instrucciones
+	printf("cual es tu puntaje:");
+	scanf("%i",&edades);
+	enegiaPersonaje--;
+	operacion=edades+30;
+	printf(":\n %.2f",operacion);
+	
+	return 0;
+	
+	
+}
